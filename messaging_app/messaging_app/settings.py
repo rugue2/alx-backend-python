25d2cc972bf48805f22b92de8bd80f4f8dc6446c
rugue2 <osarugueenehizena3@gmail.com>
2025-09-24 01:31:14 +0000

    Refactor Django REST Framework settings: standardize quotes for consistency

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -53,11 +53,11 @@ MIDDLEWARE = [
 
 # Django REST Framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
     ],
 }
 
