dd2108f98858a192f32bdc5ba28496accb7997cd
rugue2 <osarugueenehizena3@gmail.com>
2025-09-24 01:45:16 +0000

    Add serializers for User, Message, and Conversation models

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import User, Conversation, Message
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 
+                 'phone_number', 'role', 'created_at']
+        read_only_fields = ['user_id', 'created_at']
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+        read_only_fields = ['message_id', 'sent_at']
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+    participant_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        write_only=True,
+        queryset=User.objects.all(),
+        source='participants'
+    )
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'messages', 
+                 'participant_ids', 'created_at']
+        read_only_fields = ['conversation_id', 'created_at']
+
+    def validate_participant_ids(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError(
+                "A conversation must have at least 2 participants"
+            )
+        return value
