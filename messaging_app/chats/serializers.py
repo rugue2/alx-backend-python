de51e051b375dab0a58443d64637827d432294f1
rugue2 <osarugueenehizena3@gmail.com>
2025-09-24 01:47:43 +0000

    Enhance serializers: add full_name to UserSerializer and implement last_message and participant_count in ConversationSerializer

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from .models import User, Conversation, Message
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+    
     class Meta:
         model = User
         fields = ['user_id', 'username', 'email', 'first_name', 'last_name', 
-                 'phone_number', 'role', 'created_at']
+                 'phone_number', 'role', 'created_at', 'full_name']
         read_only_fields = ['user_id', 'created_at']
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -19,6 +21,9 @@ class MessageSerializer(serializers.ModelSerializer):
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True, read_only=True)
     messages = MessageSerializer(many=True, read_only=True)
+    last_message = serializers.SerializerMethodField()
+    participant_count = serializers.SerializerMethodField()
+    conversation_title = serializers.CharField(source='__str__', read_only=True)
     participant_ids = serializers.PrimaryKeyRelatedField(
         many=True,
         write_only=True,
@@ -38,3 +43,14 @@ class ConversationSerializer(serializers.ModelSerializer):
                 "A conversation must have at least 2 participants"
             )
         return value
+
+    def get_last_message(self, obj):
+        """Get the most recent message in the conversation"""
+        last_message = obj.messages.order_by('-sent_at').first()
+        if last_message:
+            return MessageSerializer(last_message).data
+        return None
+
+    def get_participant_count(self, obj):
+        """Get the number of participants in the conversation"""
+        return obj.participants.count()
